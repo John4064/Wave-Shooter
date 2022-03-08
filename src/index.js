75addf06cdc68a6fa051e6fb72f7894bf6495288
John Parkhurst <jparkhurst120@gmail.com>
2022-03-08 14:02:26 -0500

    fixing sound file

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import 'phaser';
 //Globals
 var music;
 
-class player{
+class player {
     /**
      * @param color This determines what color your "avatar" will be (either hex or rgb)
      * @brief Constructor for the player class
      */
-    player(color){
+    player(color) {
         this.color = color;
         this.hp = 100;
     }
@@ -18,16 +18,17 @@ class player{
      * @brief Just a accessor method that returns hp value of player
      * @returns the health point value of the player at this current time
      */
-    getHP(){
+    getHP() {
         return this.hp;
     }
-};
+}
 
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
     width: 800,
     height: 600,
+    backgroundColor: '#4a9bd0',
     scene: {
         preload: preload,
         create: create,
@@ -37,25 +38,24 @@ var config = {
 };
 
 
-function preload () {
+function preload() {
     this.load.image('logo', '../src/assets/img/logo.png');
-    game.load.audio('sky', '../src/assets/sound/sky.wav');
+    this.load.audio('sky', '../src/assets/sound/sky.ogg');
 
-};
+}
 var game = new Phaser.Game(config);
 
 
-function create () {
-    var logo = this.add.image(400, 150, 'logo');
+function create() {
+    var logo = this.add.image(400, 250, 'logo');
     var group = this.add.group();
-    game.stage.backgroundColor = '#4a9bd0';
-    music = game.add.audio('sky');
+    //Error occurs on line 53
+    music = this.add.audio('sky');
 
     music.play();
 
 
-
-    group.createMultiple({ key: 'raster', repeat: 64 });
+    group.createMultiple({key: 'raster', repeat: 64});
 
     var hsv = Phaser.Display.Color.HSVColorWheel();
 
@@ -76,8 +76,8 @@ function create () {
         _this.tweens.add({
             targets: child,
             props: {
-                y: { value: 500, duration: 1500 },
-                scaleX: { value: child.depth / 64, duration: 6000, hold: 2000, delay: 2000 }
+                y: {value: 500, duration: 1500},
+                scaleX: {value: child.depth / 64, duration: 6000, hold: 2000, delay: 2000}
             },
             yoyo: true,
             repeat: -1,
@@ -86,12 +86,12 @@ function create () {
         });
 
     });
-};
+}
 
-function update(){
+function update() {
     console.log(5);
-};
+}
 
-function render(){
+function render() {
     console.log(5);
-};
+}
