841c942b7ba98a371020ed681b8d2e8b5bfcf617
John Parkhurst <jparkhurst120@gmail.com>
2022-03-08 00:47:05 -0500

    Player class base

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import 'phaser';
 
+var hp = 100;
+
+
+class player{
+    /**
+     * @param color This determines what color your "avatar" will be (either hex or rgb)
+     * @brief Constructor for the player class
+     */
+    player(color){
+        this.color = color;
+        this.hp = 100;
+    }
+
+    /**
+     * @brief Just a accessor method that returns hp value of player
+     * @returns the health point value of the player at this current time
+     */
+    getHP(){
+        return this.hp;
+    }
+
+
+}
 var config = {
     type: Phaser.AUTO,
     parent: 'phaser-example',
@@ -30,5 +53,7 @@ function create ()
         yoyo: true,
         loop: -1
     });
+
+
 
 }
